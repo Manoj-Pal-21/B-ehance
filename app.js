90fbe5afea98ad689fcf8c48af423475e581c639
Manoj <manojpal211999@gmail.com>
2021-11-25 03:48:44 +0530

    functionality-complete3

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = process.env.port || 5900;
+const port = process.env.PORT || 5900;
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static('build'));
