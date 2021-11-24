764d48b39243e110d499e67615fbb9ebc2f2f417
Manoj <manojpal211999@gmail.com>
2021-11-25 03:32:24 +0530

    functionality-complete3

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const app = express();
 const path = require('path');
 const port = process.env.port || 5900;
 
-if(process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
     app.use(express.static('build'));
     app.get("*", (req, res) => {
-        req.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+        res.sendFile(path.join(__dirname, 'build/index.html'));
     });
 }
 
 app.listen(port, (err) => {
-    if(err) return console.log(err);
+    if (err) return console.log(err);
     console.log(`Server is running on port ${port}`);
 });
